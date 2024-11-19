2002309ed6769710cd69cd18358a590eaf5ad9c2
csb-coding <bramesophia@gmail.com>
2024-11-18 20:40:53 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,73 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/DSC00076.jpeg"
+about_me = "My name is Sophia Brame, and I am a second year transfer student studying Physics at Georgia Tech. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/sophia-brame-850110251/"
+my_github_url = "https://github.com/csb-coding"
+my_email_address = "bramesophia@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Physics',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Expected Graduation': 'Spring 2027',
+    'GPA': '3.8'
+}
+course_data = {
+    "code":["CS 1301", "PHYS 2213", "MATH 1554", "HIST 3127"], 
+    "names":["Intro to CS", "Intro to Modern Physics", "Linear Algebra", "History of the Civil Rights Movement"], 
+    "semester_taken":["1st", "1st", "1st", "1st"],
+    "skills":["Fundamental python coding skills", "Foundational understanding of recent physics", "The basics of linear algebra and matrix operatons", "Text analysis and advanced historical processing"],
+    }
+experience_data = {
+    "Team Member at Kaldi's Coffee": (["- Worked as a general team member, fulfilling all positions as needed",
+                                                                          "- Honed in adaptability and communication skills", "- Made Mr. Krabs Proud"],"Images/IMG_2220.jpeg"),
+    "Head Barista at Crescent Coffee":(["- Largest contributor to innovation of drink flavors and promotional ideas, also led training of all volunteers",
+                                                           "- Sharpened leadership, teamwork, and innovation skills"],"Images/IMG_5407.JPG"),
+}
+
+projects_data = {
+    "Establishing Muon Lifetime": "Worked with a group to develop an experiment that measures the lifetime expectancy for muons at sea level.",
+}
+
+programming_data = {
+    "Python ": 70,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python ": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"Italian ": "🇮🇹 ",
+    "English ": "🇬🇧 ",
+    "Latin " : "🪦 "
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English ": "Fluent",
+    "Italian ": "Elementary",
+    "Latin ": "Intermediate"
+}
+leadership_data = {
+    "Member of Alpha Gamma Delta": (["- Member Class 2024, Gamma Phi Chapter"],"Images/20240819_223136_C4F52E.JPEG"),
+    "Member of Rhodes College Society of Physics": (["- General body member 2023-2024, designated hovercraft operator"], "Images/IMG_0307.jpeg")
+
+}
+activity_data = {
+    "Second Helping Atlanta": ["- Packaged fresh ingredients for meals for families experiencing food insecurity across Atlanta", 
+            "- Learn more here: https://secondhelpingsatlanta.org/"]
+}
